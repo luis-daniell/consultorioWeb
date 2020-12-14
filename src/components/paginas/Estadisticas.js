cbff0fde46b10b438e9500c296cbbc3befef8f23
luis.daniel.tic@gmail.com <luis.daniell.tics@gmail.com>
2020-12-13 22:38:52 -0600

    13/12/2020

--- src/components/paginas/Estadisticas.js
+++ src/components/paginas/Estadisticas.js
@@ -1,14 +1,298 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
+import Chart from "react-google-charts";
+import {obtenerDatosCitas, obtenerDatosRecetas,  obtenerDatosExpedientes, obtenerDiagnosticoExpedientes} from '../../helper';
+import {FirebaseContext} from '../../firebase/Auth';
 import Sidebar from "../ui/Sidebar";
+import Barra from "../ui/Barra";
+import {useFormik} from 'formik';
+
+
 
 export const Estadisticas = () => {
+
+    const [estado, guardarEstado] = useState(false);
+    //const [yearr, guardarYear] = useState('');
+
+    const {firebase} = useContext(FirebaseContext);
+
+
+    //Año en LocalStorage
+    let year = JSON.parse(localStorage.getItem('year'));
+    if(!year){
+        year = '';
+    }
+
+
+    const [citas, guardarCitas] = useState([]);
+    const [expedientes, guardarExpedientes] = useState([]);
+    const [recetas, guardarRecetas] = useState([]);
+     
+    
+    useEffect(() => {
+        if(year === ''){
+            guardarEstado(true);
+        }else{
+            //guardarYear(year);
+            guardarEstado(false);
+        }
+
+
+        const obtenerCitas =  () => {
+            firebase.db.collection('citas').where('atendida','==',true).where('yearCita','==',year.toString()).onSnapshot(manejarSnapshot);//Snapshot para ver los cambios en tiempo real y get para ver solamnente los cambios
+            
+        }
+        obtenerCitas();
+
+        const obtenerExpedientes =  () => {
+            firebase.db.collection('expedientes').where('yearExpediente','==',year.toString()).onSnapshot(manejarSnapshot2);//Snapshot para ver los cambios en tiempo real y get para ver solamnente los cambios
+            
+        }
+        obtenerExpedientes();
+
+
+        const obtenerRecetas =  () => {
+            firebase.db.collection('recetas').where('yearReceta','==',year.toString()).onSnapshot(manejarSnapshot3);//Snapshot para ver los cambios en tiempo real y get para ver solamnente los cambios
+            
+        }
+        obtenerRecetas();
+
+
+
+        
+    },[year, firebase]);
+
+
+
+    //Snapshop nos permite usar la base de datos en tiempo real de firestore
+    function manejarSnapshot(snapshot) {
+        const citass = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        //Almacenar los resultados en el state
+        guardarCitas(citass);
+    }
+
+
+    //Snapshop nos permite usar la base de datos en tiempo real de firestore
+    function manejarSnapshot2(snapshot) {
+        const expedientess = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        //Almacenar los resultados en el state
+        guardarExpedientes(expedientess);
+    }
+
+
+    //Snapshop nos permite usar la base de datos en tiempo real de firestore
+    function manejarSnapshot3(snapshot) {
+        const recetass = snapshot.docs.map(doc => {
+            return{
+                id: doc.id,
+                ...doc.data()
+            }
+        });
+
+        //Almacenar los resultados en el state
+        guardarRecetas(recetass);
+    }
+
+
+
+
+
+
+
+    const graficaCitas = obtenerDatosCitas(citas, 'Citas');
+    const graficaExpedientes = obtenerDatosExpedientes(expedientes, 'Expedientes');
+    const graficaRecetas = obtenerDatosRecetas(recetas, 'Recetas');
+    const diagnosticos = obtenerDiagnosticoExpedientes(expedientes, 'Expedientes');
+    //console.log(diagnosticos);    
+
+
+ 
+    const formik = useFormik({
+        initialValues : {
+            year: '',
+        },
+
+
+        onSubmit: dato => {
+            
+            localStorage.setItem('year',(dato.year));
+            guardarEstado(false);
+
+        }
+
+    });
+
+
+
     return ( 
         <div className="">
             <Sidebar/>
-            <div className="bg-colorFondo w-4/5 box-border left-auto float-right">
+            
+            <div className="bg-colorFondo w-4/5 box-border left-auto float-right h-screen">
+                <Barra/>
 
+                <div className=" flex">
+
+                    <div className=" w-1/2 flex justify-start items-center">
+                        <p className="font-source content-center text-2xl font-bold pl-12 pt-6">Estadísticas</p>
+                    </div>
+
+                    
+                </div>
+
+            
+                
+                {estado ? 
+                <div className="w-11/12 flex justify-items-center items-center justify-center mt-10">
+
+                    <form 
+                        className="w-11/12 items-center"
+                        //onSubmit={handleSubmit}
+                        onSubmit={formik.handleSubmit}
+                        >
+                        <label className="w-2/4 text-tercerColor font-bold font-source text-3xl">Año: </label>
+                        <select
+                            className="ml-4 shadow appearance-none border rounded w-3/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="year"
+                            required
+                            value={formik.values.year}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        >
+                            <option value="">-- Seleccione el año --</option>
+                            <option value="2020">2020</option>
+                            <option value="2021">2021</option>
+
+                        </select>
+
+
+
+                        <button
+                            className="ml-4 bg-tercerColor hover:bg-blue-dark text-white px-4 rounded-full cursor-pointer font-source w-40 h-8"  
+                            type="submit"
+                        >
+                        Guardar</button>
+
+                    </form>
+                </div>
+                : 
+
+
+                <div className="w-11/12 flex justify-items-center items-center justify-center mt-10">
+
+                    <div className="w-11/12 flex items-center">
+
+                        <div className="w-3/12 flex justify-center items-center justify-items-center">
+                            <p className="text-tercerColor font-source text-3xl font-bold">{year} </p>
+                        </div>
+                        
+
+                        <button
+                            className="bg-tercerColor hover:bg-blue-dark text-white px-4 rounded-full cursor-pointer font-source w-40 h-8"  
+                            onClick={() => guardarEstado(true)}
+                        >
+                        Cambiar Año</button>
+                    </div>
+                </div>
+                }
+
+
+                <div className="flex justify-center items-center justify-items-center ">
+                    <div className="bg-red-700 flex">
+
+                        <Chart
+                            width={500}
+                            height={300}
+                            chartType="ColumnChart"
+                            loader={<div>Loading Chart</div>}
+                            data={graficaCitas}
+                            options={{
+                            title: 'Citas Atendidas',
+                            chartArea: { width: '80%' },
+                            hAxis: {
+                                title: 'Citas Atendidas',
+                                minValue: 0,
+                            },
+                            vAxis: {
+                                title: 'Citas',
+                            },
+                            }}
+                            legendToggle
+                        />
+                    </div>
+
+                    <div className="bg-gray-700 flex">    
+                    
+                    <Chart
+                        width={'500px'}
+                        height={'300px'}
+                        chartType="AreaChart"
+                        loader={<div>Loading Chart</div>}
+                        data={graficaExpedientes}
+                        options={{
+                            title: 'Expedientes Registrados',
+                            hAxis: { title: 'Expedientes', titleTextStyle: { color: '#333' } },
+                            vAxis: { minValue: 0 },
+                            // For the legend to fit, we make the chart area smaller
+                            chartArea: { width: '80%', height: '70%' },
+                            // lineWidth: 25
+                        }}
+                        // For tests
+                        rootProps={{ 'data-testid': '1' }}
+                    />
+                    </div>
+
+                </div>
+
+
+                <div className="flex justify-center items-center justify-items-center ">
+                    <div className="bg-red-700 flex">
+
+                    <Chart
+                        width={'500px'}
+                        height={'300px'}
+                        chartType="PieChart"
+                        loader={<div>Loading Chart</div>}
+                        data={graficaRecetas}
+                        options={{
+                            title: 'Recetas Creadas',
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                        />
+                    </div>
+
+                    <div className="bg-gray-700 flex">    
+                    
+                        <Chart
+                            width={'500px'}
+                            height={'300px'}
+                            chartType="PieChart"
+                            loader={<div>Loading Chart</div>}
+                            data={diagnosticos}
+                            options={{
+                                title: 'Diagnósticos',
+                            }}
+                            rootProps={{ 'data-testid': '1' }}
+                        />
+
+                    </div>
+
+                </div>
+
+
+                
             </div>
         </div>
-        
-     );
+  );
 }
--- src/helper.js
+++ src/helper.js
@@ -22,92 +22,73 @@ const styles = StyleSheet.create({
 });
 
 
-export function obtenerToken(accessToken) {
+export function agregarEvento (nombre, apellidos, correo, fecha, hora, descripcion ){
 
-    //let accessToken;
-    var gapi = window.gapi
-    /* 
-      Update with your own Client Id and Api key 
-    */
-    var CLIENT_ID = "529094084148-em988n3ck312m1g82k3ucm96vudp5ou2.apps.googleusercontent.com"
-    var API_KEY = "AIzaSyC5rozZvL1yXtj51_ThlbCFOxAMqDiXcIY"
-    var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
-    var SCOPES = "https://www.googleapis.com/auth/calendar"
+     
+  var gapi = window.gapi
   
-    
+  //  Update with your own Client Id and Api key 
   
-      gapi.load('client:auth2', () => {
-        console.log('loaded client')
-  
-        gapi.client.init({
-          apiKey: API_KEY,
-          clientId: CLIENT_ID,
-          discoveryDocs: DISCOVERY_DOCS,
-          scope: SCOPES,
-        })
-  
-        gapi.client.load('calendar', 'v3', () => console.log('bam!'))
-        
-  
-        gapi.auth2.getAuthInstance().signIn()
-        .then(() => {
-          
-          var event = {
-            'summary': 'Acceso al calendario',
-            'description': 'Se ha agregado este evento como prueba, lo puedes elimianr',
-            'start': {
-              'dateTime': '2020-11-27T09:00:00-07:00',
-              'timeZone': 'America/Los_Angeles'
-            },
-            'end': {
-              'dateTime': '2020-11-27T09:15:00-07:00',
-              'timeZone': 'America/Los_Angeles'
-            },
-            'recurrence': [
-              'RRULE:FREQ=DAILY;COUNT=2'
-            ],
-            'attendees': [
-              {'email': 'lpage@example.com'},
-              {'email': 'sbrin@example.com'}
-            ],
-            'reminders': {
-              'useDefault': false,
-              'overrides': [
-                {'method': 'email', 'minutes': 24 * 60},
-                {'method': 'popup', 'minutes': 10}
-              ]
-            }
-          }
-          
-          var request = gapi.client.calendar.events.insert({
-            'calendarId': 'primary',
-            'resource': event,
-          })
-  
-          request.execute(event => {
-            //console.log(event)
-            const token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse();
-            accessToken = token.access_token;
-            //console.log(accessToken);
-            
-            //window.open(event.htmlLink)
-          })
-          
-  
+  var CLIENT_ID = "529094084148-em988n3ck312m1g82k3ucm96vudp5ou2.apps.googleusercontent.com"
+  var API_KEY = "AIzaSyC5rozZvL1yXtj51_ThlbCFOxAMqDiXcIY"
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"]
+  var SCOPES = "https://www.googleapis.com/auth/calendar"
 
-          
+  //https://www.youtube.com/watch?v=zrLf4KMs71E
 
-        })//FIN DE ONCLICK
+    gapi.load('client:auth2', () => {
+      console.log('loaded client')
 
-
-        
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
       })
 
-      //console.log(accessToken);
-      return accessToken;     
-      //console.log(accessToken);
-    //console.log(accessToken);
-    //return accessToken;
+      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+      
+
+      gapi.auth2.getAuthInstance().signIn()
+      .then(() => {
+        
+        var event = {
+          'summary': `Cita a: ${nombre} ${apellidos}`,
+          'description': `${descripcion}`,
+          'start': {
+            'dateTime': `${fecha}T${hora}:00-06:00`,
+            'timeZone': 'America/Mexico_City'
+          },
+          'end': {
+            'dateTime': `${fecha}T${hora}:00-06:00`,
+            'timeZone': 'America/Mexico_City'
+          },
+          'attendees': [
+            {'email': `${correo}`}
+          ],
+          'reminders': {
+            'useDefault': false,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10}
+            ]
+          }
+        }
+        
+        var request = gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event,
+        })
+
+        request.execute(event => {
+          console.log(event)
+          const token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse();
+          console.log(token);
+          
+        })
+      })//FIN DE ONCLICK
+    })
+ 
 }
 
 
@@ -122,7 +103,6 @@ export function PdfDocument(props) {
 //Se configuro en google cloud
  
   return (
-
     <Document>
       <Page size="A4" wrap >
           <Text style={styles.title}>{props.data.nombreConsultorio}</Text>
@@ -130,31 +110,419 @@ export function PdfDocument(props) {
           
       </Page>
     </Document>
-
-
-
-    /*
-    <Document>
-      <Page >
-        {data
-          ? data.map((a, index) => {
-              return (
-                <View key={index} >
-                  
-                  <View >
-                    <Text >{data}</Text>
-                     
-                     
-                  </View>
-                </View>
-              );
-            })
-          : ""}
-      </Page>
-    </Document>*/
   );
-
-
-  
-  
 }
+
+export function obtenerDatosCitas(citas, titulo){
+  let enero = 0;
+        let febrero = 0;
+        let marzo = 0;
+        let abril = 0;
+        let mayo = 0;
+        let junio = 0;
+        let julio = 0;
+        let agosto = 0;
+        let septiembre = 0;
+        let octubre = 0;
+        let noviembre = 0;
+        let diciembre = 0;
+    //console.log(citas);
+
+    citas.forEach(logArrayElements);
+    
+
+    function logArrayElements(element, index, array) {
+
+        
+
+        if(element.mesCita === "01"){
+            enero = enero + 1;
+
+        }else if(element.mesCita === "02"){
+            febrero = febrero + 1;
+
+        }else if(element.mesCita === "03"){
+            marzo = marzo + 1;
+
+        }else if(element.mesCita === "04"){
+            abril = abril + 1;
+
+        }else if(element.mesCita === "05"){
+            mayo = mayo + 1;
+
+        }else if(element.mesCita === "06"){
+            junio = junio + 1;
+
+        }else if(element.mesCita === "07"){
+            julio = julio + 1;
+
+        }else if(element.mesCita === "08"){
+            agosto = agosto + 1;
+
+        }else if(element.mesCita === "09"){
+            septiembre = septiembre + 1;
+
+        }else if(element.mesCita === "10"){
+            octubre = octubre + 1;
+
+        }else if(element.mesCita === "11"){
+            noviembre = noviembre + 1;
+
+        }else{
+            diciembre = diciembre + 1;
+        }
+
+    }
+
+    const meses = [{
+        mes: 'Enero',
+        cantidad: enero
+        },{
+        mes: 'Febrero',
+        cantidad: febrero
+        }, {
+        mes: 'Marzo',
+        cantidad: marzo
+        },
+        {
+        mes: 'Abril',
+        cantidad: abril
+        },
+        {
+        mes: 'Mayo',
+        cantidad: mayo
+        }, {
+        mes: 'Junio',
+        cantidad: junio
+        }, {
+        mes: 'Julio',
+        cantidad: julio
+        }, {
+        mes: 'Agosto',
+        cantidad: agosto
+        }, {
+        mes: 'Septiembre',
+        cantidad: septiembre
+        }, {
+        mes: 'Octubre',
+        cantidad: octubre
+        }, {
+        mes: 'Noviembre',
+        cantidad: noviembre
+        }, {
+        mes: 'Diciembre',
+        cantidad: diciembre
+        }
+
+
+    ];
+
+
+   // console.log(meses);
+    const returnedArray = [[titulo, '']];
+    for(let i=0; i<meses.length; i++) {
+
+        if(meses[i].cantidad >= 1){
+            returnedArray[i+1] = [meses[i].mes, meses[i].cantidad];
+
+
+        }
+            
+    } 
+
+
+    var filtered = returnedArray.filter(function (el) {
+        return el != null;
+      });
+
+
+      return filtered;
+}
+
+
+export function obtenerDatosExpedientes(expedientes, titulo){
+  let enero = 0;
+        let febrero = 0;
+        let marzo = 0;
+        let abril = 0;
+        let mayo = 0;
+        let junio = 0;
+        let julio = 0;
+        let agosto = 0;
+        let septiembre = 0;
+        let octubre = 0;
+        let noviembre = 0;
+        let diciembre = 0;
+    //console.log(expedientes);
+
+    expedientes.forEach(logArrayElements);
+    
+
+    function logArrayElements(element, index, array) {
+
+        
+
+        if(element.mesExpediente === "01"){
+            enero = enero + 1;
+
+        }else if(element.mesExpediente === "02"){
+            febrero = febrero + 1;
+
+        }else if(element.mesExpediente === "03"){
+            marzo = marzo + 1;
+
+        }else if(element.mesExpediente === "04"){
+            abril = abril + 1;
+
+        }else if(element.mesExpediente === "05"){
+            mayo = mayo + 1;
+
+        }else if(element.mesExpediente === "06"){
+            junio = junio + 1;
+
+        }else if(element.mesExpediente === "07"){
+            julio = julio + 1;
+
+        }else if(element.mesExpediente === "08"){
+            agosto = agosto + 1;
+
+        }else if(element.mesExpediente === "09"){
+            septiembre = septiembre + 1;
+
+        }else if(element.mesExpediente === "10"){
+            octubre = octubre + 1;
+
+        }else if(element.mesExpediente === "11"){
+            noviembre = noviembre + 1;
+
+        }else{
+            diciembre = diciembre + 1;
+        }
+
+    }
+
+    const meses = [{
+        mes: 'Enero',
+        cantidad: enero
+        },{
+        mes: 'Febrero',
+        cantidad: febrero
+        }, {
+        mes: 'Marzo',
+        cantidad: marzo
+        },
+        {
+        mes: 'Abril',
+        cantidad: abril
+        },
+        {
+        mes: 'Mayo',
+        cantidad: mayo
+        }, {
+        mes: 'Junio',
+        cantidad: junio
+        }, {
+        mes: 'Julio',
+        cantidad: julio
+        }, {
+        mes: 'Agosto',
+        cantidad: agosto
+        }, {
+        mes: 'Septiembre',
+        cantidad: septiembre
+        }, {
+        mes: 'Octubre',
+        cantidad: octubre
+        }, {
+        mes: 'Noviembre',
+        cantidad: noviembre
+        }, {
+        mes: 'Diciembre',
+        cantidad: diciembre
+        }
+
+
+    ];
+
+
+   // console.log(meses);
+    const returnedArray = [[titulo, '']];
+    for(let i=0; i<meses.length; i++) {
+
+        if(meses[i].cantidad >= 1){
+            returnedArray[i+1] = [meses[i].mes, meses[i].cantidad];
+
+
+        }
+            
+    } 
+
+
+    var filtered = returnedArray.filter(function (el) {
+        return el != null;
+      });
+
+
+      return filtered;
+}
+
+
+export function obtenerDatosRecetas(recetas, titulo){
+  let enero = 0;
+        let febrero = 0;
+        let marzo = 0;
+        let abril = 0;
+        let mayo = 0;
+        let junio = 0;
+        let julio = 0;
+        let agosto = 0;
+        let septiembre = 0;
+        let octubre = 0;
+        let noviembre = 0;
+        let diciembre = 0;
+    //console.log(recetas);
+
+    recetas.forEach(logArrayElements);
+    
+
+    function logArrayElements(element, index, array) {
+
+        
+
+        if(element.mesReceta === "01"){
+            enero = enero + 1;
+
+        }else if(element.mesReceta === "02"){
+            febrero = febrero + 1;
+
+        }else if(element.mesReceta === "03"){
+            marzo = marzo + 1;
+
+        }else if(element.mesReceta === "04"){
+            abril = abril + 1;
+
+        }else if(element.mesReceta === "05"){
+            mayo = mayo + 1;
+
+        }else if(element.mesReceta === "06"){
+            junio = junio + 1;
+
+        }else if(element.mesReceta === "07"){
+            julio = julio + 1;
+
+        }else if(element.mesReceta === "08"){
+            agosto = agosto + 1;
+
+        }else if(element.mesReceta === "09"){
+            septiembre = septiembre + 1;
+
+        }else if(element.mesReceta === "10"){
+            octubre = octubre + 1;
+
+        }else if(element.mesReceta === "11"){
+            noviembre = noviembre + 1;
+
+        }else{
+            diciembre = diciembre + 1;
+        }
+
+    }
+
+    const meses = [{
+        mes: 'Enero',
+        cantidad: enero
+        },{
+        mes: 'Febrero',
+        cantidad: febrero
+        }, {
+        mes: 'Marzo',
+        cantidad: marzo
+        },
+        {
+        mes: 'Abril',
+        cantidad: abril
+        },
+        {
+        mes: 'Mayo',
+        cantidad: mayo
+        }, {
+        mes: 'Junio',
+        cantidad: junio
+        }, {
+        mes: 'Julio',
+        cantidad: julio
+        }, {
+        mes: 'Agosto',
+        cantidad: agosto
+        }, {
+        mes: 'Septiembre',
+        cantidad: septiembre
+        }, {
+        mes: 'Octubre',
+        cantidad: octubre
+        }, {
+        mes: 'Noviembre',
+        cantidad: noviembre
+        }, {
+        mes: 'Diciembre',
+        cantidad: diciembre
+        }
+
+
+    ];
+
+
+   // console.log(meses);
+    const returnedArray = [[titulo, '']];
+    for(let i=0; i<meses.length; i++) {
+
+        if(meses[i].cantidad >= 1){
+            returnedArray[i+1] = [meses[i].mes, meses[i].cantidad];
+        }    
+    } 
+    var filtered = returnedArray.filter(function (el) {
+        return el != null;
+      });
+
+
+      return filtered;
+}
+
+
+export function obtenerDiagnosticoExpedientes(expedientes, titulo){
+ 
+  const returnedArray = [[titulo, '']];  
+  const arreglo = [];
+  expedientes.forEach(logArrayElements);
+    
+    function logArrayElements(element, index, array) {
+
+      arreglo.push(element.diagnostico);
+
+    }
+    
+    var repetidos = {};
+
+    arreglo.forEach(function(numero){
+      repetidos[numero] = (repetidos[numero] || 0) + 1;
+    });
+
+    const llaves = Object.keys(repetidos);
+    const valores = Object.values(repetidos);
+
+    for(let i=0; i<llaves.length; i++) {
+
+      returnedArray[i+1] = [llaves[i], valores[i]];
+      
+  }
+      return returnedArray;
+}
+
+
+
+
+
+
+
+
+
